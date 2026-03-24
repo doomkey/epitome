17f5f0c6b6ed48f8a5b9cd06b39acf9924ba9d5a
Imran Molla Joy <imranmollajoy@gmail.com>
2026-03-24 11:35:50 +0600

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -19,7 +19,7 @@
 	];
 </script>
 
-<footer class="border-t bg-background">
+<footer class="mt-auto border-t bg-background">
 	<div class="container mx-auto px-4 py-12">
 		<div class="grid grid-cols-2 gap-8 sm:grid-cols-2 lg:grid-cols-4">
 			<div class="col-span-2 sm:col-span-2 lg:col-span-1">
