6f86d548e2bdef417b3e98ebaa13c9dfeb84ee15
Imran Molla Joy <imranmollajoy@gmail.com>
2026-03-25 15:36:43 +0600

    darkmode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,14 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import NavbarDefault from '$lib/components/NavbarDefault.svelte';
 	import { base } from '$app/paths';
+	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<meta property="og:image" content={banner} />
 </svelte:head>
+<ModeWatcher />
 <Toaster position="top-center" />
 {#if page.url.pathname !== base + '/generate'}
 	<NavbarDefault />
